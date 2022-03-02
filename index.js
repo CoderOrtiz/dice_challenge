1000347c047e199db76f88a3c665efce9f9a0b17
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-03-02 16:45:10 -0500

    refactored code and made sure grammar in game was correct

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
 // Enter Your Name
+player2 = prompt("What is your name?");
 
-player2 = prompt("What is your name?")
-alert("Welcome " + player2 + ", prepare to lose 😈")
-if(player2){
-document.querySelectorAll("p")[1].textContent = player2;
+// Welcome Message
+alert("Welcome " + player2 + ", prepare to lose! 😈");
+
+// Adds Player 2's Name Above Player Two in Game
+if (player2) {
+    document.querySelectorAll("p")[1].textContent = player2;
 }
-else{};
+else { };
 
 // When You Click, "Click Me!", The Game Begins
-function run(){
-    
-// Dice 1
-number1 = Math.floor(Math.random() * 6) + 1;
+function run() {
 
-dice1 = "./assets/images/dice" + number1 + ".png";
+    // Dice 1
+    number1 = Math.floor(Math.random() * 6) + 1;
+    dice1 = "./assets/images/dice" + number1 + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", dice1);
 
-document.querySelectorAll("img")[0].setAttribute("src", dice1);
+    //  Dice 2
+    number2 = Math.floor(Math.random() * 6) + 1;
+    dice2 = "./assets/images/dice" + number2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", dice2);
 
-//  Dice 2
-
-number2 = Math.floor(Math.random() * 6) + 1;
-
-dice2 = "./assets/images/dice" + number2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", dice2);
-
-// Selecting Winner
-
-if (number1 > number2){
-    document.querySelector("h3").textContent = "I win 😝 😎"
-}
-else if (number1 < number2){
-    document.querySelector("h3").textContent = "You Win 😭 "
-}
-else{
-    document.querySelector("h3").textContent = "Its a Draw 🪖 🏳"
-}
+    // Selecting Winner and Pronouncing Winner
+    if (number1 > number2) {
+        document.querySelector("h3").textContent = "Skynet wins 😝 😎";
+    }
+    else if (number1 < number2) {
+        document.querySelector("h3").textContent = player2 + " Wins 😭 ";
+    }
+    else {
+        document.querySelector("h3").textContent = "Its a Draw 🪖 🏳";
+    }
 
 }
