cc7c726dc877aa6d9038f386633f1155ea030534
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-03-04 19:54:20 -0500

    if user hits cancel when prompt is displayed, 'player 2' wins will still be displayed

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // Enter Your Name
 player = prompt("What is your name?");
 
+// This Ensures if "Player 2 Wins" will be Displayed, if User Hits "Cancel" when Prompt is Displayed
+player = player || "Player 2";
+
 // Capitalize's the First Letter & Decapitalizes the remaining letters
 namePart1 = player.slice(0, 1);
 namePart2 = player.slice(1, 15);
 
 // Adds "namePart1" & "namePart2" to Make the Player's Name Grammatically Correct
-player2 = namePart1.toUpperCase() + namePart2.toLowerCase() || "Player 2"
+player2 = namePart1.toUpperCase() + namePart2.toLowerCase() || "Player 2";
 
 // Welcome Message
 alert("Welcome " + player2 + ", prepare to lose! 😈");
@@ -35,7 +38,7 @@ function run() {
         document.querySelector("h3").textContent = "Skynet wins 😝 😎";
     }
     else if (number1 < number2) {
-        document.querySelector("h3").textContent = player2 + " Wins 😭 ";
+        document.querySelector("h3").textContent = player2 + " Wins 😭";
     }
     else {
         document.querySelector("h3").textContent = "Its a Draw 🪖 🏳";
